2e83905a0fbd88edbc8c6ed43496382a2f7cd2c1
uestc-zb <96124033+uestc-zb@users.noreply.github.com>
2024-11-08 04:14:12 +0000

    update

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +14,11 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let mut res = first.to_uppercase().to_string();
+            res.extend(c);
+            res
+        }
     }
 }
 
@@ -24,7 +27,8 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let vec: Vec<String> = words.iter().map(|s| capitalize_first(s)).collect();
+    vec
 }
 
 // Step 3.
@@ -32,7 +36,10 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let vec = capitalize_words_vector(words);
+    
+    vec.join("")
+    
 }
 
 #[cfg(test)]
